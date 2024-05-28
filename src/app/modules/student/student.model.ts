54425b51cd57138ced94ef7a835060815442cfa5
belaeat <omyhossain@gmail.com>
2024-05-29 01:09:02 +0300

    student model has been updated

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -13,44 +13,108 @@ import {
 // creating instance from Schema class
 
 const userNameSchema = new Schema<UserName>({
-  firstName: { type: String, required: true },
+  firstName: { type: String, required: [true, 'First name is required'] },
   middleName: { type: String },
-  lastName: { type: String, required: true },
+  lastName: { type: String, required: [true, 'Last name is required'] },
 });
 
 const guardianSchema = new Schema<Guardian>({
-  fathersName: { type: String, required: true },
-  fathersOccupation: { type: String, required: true },
-  fathersContactNo: { type: String, required: true },
-  mothersName: { type: String, required: true },
-  mothersOccupation: { type: String, required: true },
-  mothersContactNo: { type: String, required: true },
+  fathersName: {
+    type: String,
+    required: [true, 'Fathers name is required'],
+  },
+  fathersOccupation: {
+    type: String,
+    required: [true, 'Fathers occupation is required'],
+  },
+  fathersContactNo: {
+    type: String,
+    required: [true, 'Fathers contact no is required'],
+  },
+  mothersName: {
+    type: String,
+    required: [true, 'Mothers name is required'],
+  },
+  mothersOccupation: {
+    type: String,
+    required: [true, 'Mothers occupation is required'],
+  },
+  mothersContactNo: {
+    type: String,
+    required: [true, 'Mothers contact no is required'],
+  },
 });
 
 const localGuardianSchema = new Schema<LocalGuardian>({
-  name: { type: String, required: true },
-  occupation: { type: String, required: true },
-  contactNo: { type: String, required: true },
-  address: { type: String, required: true },
+  name: { type: String, required: [true, 'Local guardians name is required'] },
+  occupation: {
+    type: String,
+    required: [true, 'Local guardians occupation is required'],
+  },
+  contactNo: {
+    type: String,
+    required: [true, 'Local guardians contact no is required'],
+  },
+  address: {
+    type: String,
+    required: [true, 'Local guardians address is required'],
+  },
 });
 
 // creating schema
 
 const studentSchema = new Schema<Student>({
   id: { type: String },
-  name: userNameSchema,
-  gender: ['male', 'female'], // eigula predefined value, tai enum type use korechi, enum type schema'r ekta special type
-  email: { type: String, required: true },
+  name: {
+    type: userNameSchema,
+    required: [true, 'Students name is required'],
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female', 'other'],
+      message:
+        "The gender field can only be one of the following : 'male','female, or 'other'",
+    },
+    required: true,
+  }, // eigula predefined value, tai enum type use korechi, enum type schema'r ekta special type
+
+  email: { type: String, required: [true, 'Students email is required'] },
   dateOfBirth: { type: String },
-  contactNumber: { type: String, required: true },
-  emergencyContactNo: { type: String, required: true },
-  bloodGroup: ['A+', ' A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
-  presentAddress: { type: String, required: true },
-  permanentAddress: { type: String, required: true },
-  guardian: guardianSchema,
-  localGuardian: localGuardianSchema,
+  contactNumber: {
+    type: String,
+    required: [true, 'Students contact number is required'],
+  },
+  emergencyContactNo: {
+    type: String,
+    required: [true, 'Students emergency contact no is required'],
+  },
+  bloodGroup: {
+    type: String,
+    enum: ['A+', ' A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  },
+  presentAddress: {
+    type: String,
+    required: [true, 'Present address is required'],
+  },
+  permanentAddress: {
+    type: String,
+    required: [true, 'Permanent address is required'],
+  },
+  guardian: {
+    type: guardianSchema,
+    required: [true, 'Guardians detail is required'],
+  },
+  localGuardian: {
+    type: localGuardianSchema,
+    required: [true, 'Local guardians detail is required'],
+  },
   profileImage: { type: String },
-  isActive: ['active', 'blocked'],
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+  },
 });
 
 // Step 3: Creating model
